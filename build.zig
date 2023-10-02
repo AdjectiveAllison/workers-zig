e7aa6957be0ebd8f20d4c4b2f372fc126f03d21a
Allison Durham <AdjectiveAllison@gmail.com>
2023-10-02 16:54:47 -0500

    Updated build.zig for current zig master

--- build.zig
+++ build.zig
@@ -1,59 +1,52 @@
 const std = @import("std");
-const Builder = std.build.Builder;
+const LibraryOptions = std.Build.SharedLibraryOptions;
 
-pub fn build(b: *Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-    const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
-    b.is_release = true;
-    const mode = b.standardReleaseOptions();
-    b.cache_root = "cache";
-    b.global_cache_root = "cache";
-    b.use_stage1 = true;
+pub fn build(b: *std.Build) void {
+    const wasm_build_options = LibraryOptions{
+        .name = "zig",
+        .root_source_file = std.Build.LazyPath.relative("lib/main.zig"),
+        .optimize = std.builtin.OptimizeMode.ReleaseSmall,
+        .target = std.zig.CrossTarget{
+            .cpu_arch = .wasm32,
+            .os_tag = .freestanding,
+        },
+        //.main_pkg_path = std.Build.LazyPath.relative("lib"),
+    };
 
     // ensure main is building
-    const wasm_build = b.addSharedLibrary("zig", "lib/main.zig", .unversioned);
-    wasm_build.setTarget(std.zig.CrossTarget {
-        .cpu_arch = .wasm32,
-        .os_tag = .freestanding,
-    });
-    wasm_build.linkage = std.build.LibExeObjStep.Linkage.dynamic;
-    wasm_build.install();
+    const wasm_build = b.addSharedLibrary(wasm_build_options);
+    b.installArtifact(wasm_build);
 
+    const tests_build_options = LibraryOptions{
+        .name = "zigWASM",
+        .root_source_file = std.Build.LazyPath.relative("lib/tests.zig"),
+        .optimize = std.builtin.OptimizeMode.ReleaseSmall,
+        .target = std.zig.CrossTarget{
+            .cpu_arch = .wasm32,
+            .os_tag = .freestanding,
+        },
+    };
     // build tests for freestanding wasm
-    const tests_build = b.addSharedLibrary("zigWASM", "lib/tests.zig", .unversioned);
-    tests_build.setTarget(std.zig.CrossTarget {
-        .cpu_arch = .wasm32,
-        .os_tag = .freestanding,
-    });
-    // tests_build.setOutputDir("wasm");
-    tests_build.build_mode = std.builtin.Mode.ReleaseSmall;
-    tests_build.strip = true;
-    tests_build.linkage = std.build.LibExeObjStep.Linkage.dynamic;
-    tests_build.addPackagePath("workers-zig", "lib/main.zig");
-    tests_build.install();
+    const tests_build = b.addSharedLibrary(tests_build_options);
+
+    tests_build.addAnonymousModule("workers-zig", .{ .source_file = std.Build.LazyPath.relative("lib/main.zig") });
+
+    b.installArtifact(tests_build);
+
+    const tests_wasi_build_options = LibraryOptions{
+        .name = "zigWASI",
+        .root_source_file = std.Build.LazyPath.relative("lib/testsWASI.zig"),
+        .optimize = std.builtin.OptimizeMode.ReleaseSmall,
+        .target = std.zig.CrossTarget{ .cpu_arch = .wasm32, .os_tag = .wasi },
+    };
 
     // build tests for wasi wasm
-    const tests_wasi_build = b.addSharedLibrary("zigWASI", "lib/testsWASI.zig", .unversioned);
-    tests_wasi_build.setTarget(std.zig.CrossTarget {
-        .cpu_arch = .wasm32,
-        .os_tag = .wasi,
-    });
-    // tests_wasi_build.setOutputDir("wasm");
-    tests_wasi_build.build_mode = std.builtin.Mode.ReleaseSmall;
-    tests_wasi_build.strip = true;
-    tests_wasi_build.linkage = std.build.LibExeObjStep.Linkage.dynamic;
-    tests_wasi_build.addPackagePath("workers-zig", "lib/main.zig");
-    tests_wasi_build.install();
+    const tests_wasi_build = b.addSharedLibrary(tests_wasi_build_options);
+    tests_wasi_build.addAnonymousModule("workers-zig", .{ .source_file = std.Build.LazyPath.relative("lib/main.zig") });
 
-    const exe_tests = b.addTest("lib/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
+    b.installArtifact(tests_wasi_build);
+
+    const exe_tests = b.addTest(.{ .root_source_file = std.Build.LazyPath.relative("lib/main.zig") });
 
     const test_step = b.step("test", "Run unit tests");
     test_step.dependOn(&exe_tests.step);
